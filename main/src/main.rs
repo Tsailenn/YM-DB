9bde52da32df225203489a59ecee03e3c1415bfa
TNBruh <70084579+Tsailenn@users.noreply.github.com>
2022-02-26 18:00:25 +0800

    part 2, i guess

--- main/src/main.rs
+++ main/src/main.rs
@@ -1,7 +1,8 @@
 use std::io::Write;
 use std::io::stdin;
 use std::io::stdout;
-mod cmd;
+pub mod commands;
+use commands::token::Token;
 
 fn main() {
     loop {
@@ -10,16 +11,32 @@ fn main() {
         stdin().read_line(&mut inp).ok().expect("Failed to read line");
         inp.pop();
         
-        let res = cmd::cmd::parser(&inp);
+        let res = commands::parser(&inp);
         match res {
-            Ok(cmd::cmd::Token::Exit) => {
+            Ok(Token::Exit) => {
                 println!("Exiting...");
                 break;
             },
+            Ok(Token::Insert(arg)) => {
+                println!("Insert...");
+                println!("{}", arg);
+            },
+            Ok(Token::Delete(arg)) => {
+                println!("Delete...");
+                println!("{}", arg);
+            },
+            Ok(Token::Select(arg)) => {
+                println!("Select...");
+                println!("{}", arg);
+            },
+            Ok(Token::Cast(arg)) => {
+                println!("Cast...");
+                println!("{}", arg);
+            }
             Err(c) => {
                 println!("Unrecognized command: {}", c);
             }
-        }
+        };
     }
     // println!("{}", inp);
 }
